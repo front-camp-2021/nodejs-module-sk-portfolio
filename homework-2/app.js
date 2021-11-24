5f77cce5de4456bbd93555293be461d75f04994a
Serhyi <catalonya1111@gmail.com>
2021-11-24 17:20:10 +0200

    make first and second homework

---
+++ homework-2/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+const fs = require("fs");
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Greeting from server!");
+});
+
+app.get("/data", (req, res) => {
+  res.send("Welcome to data path");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
+
+const numbersFile = "./homework-2/text/numbers.txt";
+
+const fileForReadAndDelete = "./homework-2/text/file-for-read-and-delete.txt";
+
+let numbers = "";
+
+fs.writeFile(numbersFile, numbers, (err) => {
+  if (err) throw err;
+  console.log(`${numbersFile} was created`);
+  for (let i = 1; i <= 10; i++) {
+    numbers = numbers + `${i}\n`;
+  }
+  fs.appendFileSync(numbersFile, numbers, (err) => {
+    if (err) throw err;
+  });
+  console.log(`${numbersFile} was written`);
+});
+
+fs.readFile(fileForReadAndDelete, "utf8", (err, data) => {
+  if (err)
+    console.log(`file '${fileForReadAndDelete}' not founded for reading`);
+  else console.log(`${data}`);
+});
+fs.unlink(fileForReadAndDelete, (err) => {
+  if (err)
+    console.log(`file '${fileForReadAndDelete}' not founded for removing`);
+  else console.log(`'${fileForReadAndDelete}' was removed`);
+});
